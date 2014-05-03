0545d1dde284a4abc9276f035f72df54df5eae88
jdonier <jonathan.donier@gmail.com>
2014-05-03 15:49:58 +0200

    api

---
+++ library_API.py
@@ -0,0 +1,186 @@
+import httplib, urllib
+import json
+import math as m
+import time
+
+class functions(object):
+
+	def __init__(self):
+		return None
+	
+	def call(self, values):
+		host = 'crowdguess2.herokuapp.com'#'localhost:8000'
+		url = '/API/'	
+		headers = {
+			'User-Agent': 'python',
+			'Content-Type': 'application/x-www-form-urlencoded',
+		}
+		values = urllib.urlencode(values)
+		conn = httplib.HTTPConnection(host)
+		conn.request("POST", url, values, headers)
+		response = conn.getresponse()
+		data = response.read()
+		data= json.loads(data)
+		return data
+		
+	def get_last_price(self, trades):
+		return trades[len(trades)-1]['price']
+
+	def get_price_exp_MA(self, trades, n):
+		p = 0
+		norm = 0
+		i=0
+		for trade in trades[::-1]:
+			p += m.exp(-i*1.0/n)*trade['price']
+			norm += m.exp(-i*1.0/n)
+			i+=1
+		return 0 if norm==0 else p/norm
+
+	def get_volume(self, trades):
+		volume=0
+		for trade in trades:
+			if trade.has_key('null'):
+				if not trade['null']:
+					volume += trade['volume']
+		return volume
+
+	def get_buy_volume(self, trades):
+		volume=0
+		for trade in trades:
+			if trade['side']==1:
+				if trade.has_key('null'):
+					if not trade['null']:
+						volume += trade['volume']
+				else:			
+					volume += trade['volume']
+		return volume
+		
+	def get_sell_volume(self, trades):
+		volume=0
+		for trade in trades:
+			if trade['side']==-1:
+				if trade.has_key('null'):
+					if not trade['null']:
+						volume += trade['volume']
+				else:			
+					volume += trade['volume']	
+		return volume
+
+	def get_buy_avg_price(self, trades):
+		p=0
+		norm = 0
+		for trade in trades:
+			if trade['side']==1:
+				if trade.has_key('null'):
+					if not trade['null']:
+						p += trade['volume']*trade['price']
+						norm += trade['volume']
+				else:
+					p += trade['volume']*trade['price']
+					norm += trade['volume']	
+		return 0 if norm==0 else p/norm
+		
+	def get_sell_avg_price(self, trades):
+		p=0
+		norm = 0
+		for trade in trades:
+			if trade['side'] == -1:
+				if trade.has_key('null'):
+					if not trade['null']:
+						p += trade['volume']*trade['price']
+						norm += trade['volume']
+				else:
+					p += trade['volume']*trade['price']
+					norm += trade['volume']	
+		return 0 if norm==0 else p/norm
+			
+	def get_depth(self, limits, n):
+		bid=[]
+		ask=[]
+		i=0
+		level = 0
+		while i<len(limits) and level<n:
+			if limits[i]['side'] == -1:
+				j=0
+				price = limits[i]['price']
+				volume = 0
+				ids=[]
+				while i+j<len(limits) and limits[i+j]['price']==price:
+					volume += limits[i+j]['volume']
+					ids.append(limits[i+j]['id_order'])
+					j+=1
+				level+=1	
+				ask.append({'price' : price, 'volume' : volume, 'id_orders' : ids})
+				i = i+j-1
+			i+=1
+		i = len(limits)-1
+		level = 0
+		while i>=0 and level<n:
+			if limits[i]['side'] == 1:
+				j=0
+				price = limits[i]['price']
+				volume = 0
+				ids=[]
+				while i-j>=0 and limits[i-j]['price']==price:
+					volume += limits[i-j]['volume']
+					ids.append(limits[i-j]['id_order'])
+					j+=1
+				level+=1	
+				bid.append({'price' : price, 'volume' : volume, 'id_orders' : ids})
+				i = i-j+1
+			i-=1
+		return {'bid': bid, 'ask' : ask}
+			
+	def get_related_markets(self, events, id_market):
+		is_right_event = False
+		for event in events:
+			markets = []
+			for market in event['markets']:
+				markets.append({'id_market' : market['id_market'], 'outcome' : market['outcome']})
+				if market['id_market']==id_market:
+					is_right_event=True
+			if is_right_event:
+				return markets
+		return None
+			
+	def get_markets_in_event(self, events, id_event):
+		markets=[]
+		for event in events:
+			if event['id_event']==id_event:
+				for market in event['markets']:
+					markets.append({'id_market' : market['id_market'], 'outcome' : market['outcome']})
+		return markets			
+		
+	def get_position(self, trades):
+		pos = 0
+		for trade in trades:
+			if not trade['null']:
+				if trade['side']==1:
+					pos+=trade['volume']
+				else:
+					pos -= trade['volume']
+		return pos		
+
+	def get_position_exp_MA(self, trades, n):
+		position = [0]
+		pos = 0
+		for trade in trades:
+			if not trade['null']:
+				if trade['side']==1:
+					pos+=trade['volume']
+				else:
+					pos -= trade['volume']
+				position.append(pos)	
+		p = 0
+		norm = 0
+		i=0
+		for po in position[::-1]:
+			p += m.exp(-i*1.0/n)*po
+			norm += m.exp(-i*1.0/n)
+			i+=1
+		return 0 if norm==0 else p/norm
+
+# get_balance_for_each_outcome(id_event)   => PNL par outcome si settlement immediat
+	
+
+
---
+++ send_request.py
@@ -0,0 +1,173 @@
+import library_API as la
+import time
+import math as m
+
+class Agent(object):
+
+	def __init__(self, lambda1, lambda2, tau, qty, spread, api_key, funcs):
+		self.api_key = api_key
+		self.lambda1 = lambda1
+		self.lambda2 = lambda2
+		self.tau = tau
+		self.qty = qty
+		self.spread = spread
+		self.position1 = 0
+		self.position2 = 0
+		self.pnl = 0
+		self.target = 50
+		self.trades = {}
+		self.limits = {}
+		self.mytrades = {}
+		self.mylimits = {}
+		self.balance = 0
+		self.funcs = funcs
+		
+	def HasChanged(self):
+		values = {'key' : self.api_key, 'function' : 'get_change'}
+		time.sleep(0.15)
+		data = funcs.call(values)
+		return -1 if data['status']==1 else data['change']
+	
+	def GetBalance(self):
+		values = {'key' : self.api_key, 'function' : 'get_balance'}
+		time.sleep(sleep_time)
+		data = funcs.call(values)
+		return -1 if data['status']==1 else data['balance']
+	
+	def GetTrades(self, id_market):
+		values = {'key' : self.api_key, 'function' : 'get_trades', 'id_market' : id_market}
+		time.sleep(sleep_time)
+		data = funcs.call(values)
+		self.trades = data['trades']
+		return -1 if data['status']==1 else len(data['trades'])
+	
+	def GetMyTrades(self, id_market):
+		values = {'key' : self.api_key, 'function' : 'get_my_trades', 'id_market' : id_market}
+		time.sleep(sleep_time)
+		data = funcs.call(values)
+		self.mytrades = data['trades']
+		return -1 if data['status']==1 else len(data['trades'])
+	
+	def GetLimits(self, id_market):
+		values = {'key' : self.api_key, 'function' : 'get_limits', 'id_market' : id_market}
+		time.sleep(sleep_time)
+		data = funcs.call(values)
+		self.limits=data['limits']
+		return -1 if data['status']==1 else len(data['limits'])
+	
+	def GetMyLimits(self, id_market):
+		values = {'key' : self.api_key, 'function' : 'get_my_limits', 'id_market' : id_market}
+		time.sleep(sleep_time)
+		data = funcs.call(values)
+		self.mylimits=data['limits']
+		return -1 if data['status']==1 else len(data['limits'])
+			
+	def DoIt(self, id_market):
+		last = funcs.get_last_price(self.trades)
+		buy_volume = funcs.get_buy_volume(self.mytrades)
+		sell_volume = funcs.get_sell_volume(self.mytrades)
+		self.pnl = 0.01*(sell_volume*funcs.get_sell_avg_price(self.mytrades) - buy_volume*funcs.get_buy_avg_price(self.mytrades) + (buy_volume - sell_volume)*last)
+				
+		self.position1 = buy_volume - sell_volume
+		self.position2 = self.position2 * (1.0 -  m.exp(-1.0/self.tau)) + self.position1 * m.exp(-1.0/self.tau)
+		
+		mid = self.target - self.lambda1 * self.position1 - self.lambda2 * (self.position1 - self.position2)
+		s = self.spread + self.qty * 0.5 * (self.lambda1 + self.lambda2)
+
+		buy_target_price = 0.5*m.floor(2*mid-s)
+		sell_target_price = 0.5*m.ceil(2*mid+s)
+		
+		my_first = funcs.get_depth(self.mylimits, 1)		
+		my_buy_price = -1 if len(my_first['bid'])==0 else my_first['bid'][0]['price']
+		my_sell_price = -1 if len(my_first['ask'])==0 else my_first['ask'][0]['price']
+		
+		success_send = True
+		success_cancel = True
+		sent = 0
+		canceled = 0
+			
+		if my_buy_price != buy_target_price:
+			if my_buy_price != -1:
+				for id in my_first['bid'][0]['id_orders']:
+					values = {'key' : self.api_key, 'function' : 'cancel_order', 'id_order' : id}
+					time.sleep(sleep_time)
+					data = funcs.call(values)
+					if data['status'] == 1:
+						success_cancel = False
+					else:
+						canceled += 1
+			values = {'key' : self.api_key, 'function' : 'send_order', 'id_market' : id_market, 'side' : 1, 'price' : buy_target_price, 'volume' : self.qty}	
+			time.sleep(sleep_time)
+			data = funcs.call(values)
+			if data['status'] == 1:
+				success_send = False
+			else:
+				sent += 1
+		
+		if my_sell_price != sell_target_price:
+			if my_sell_price != -1:
+				for id in my_first['ask'][0]['id_orders']:
+					values = {'key' : self.api_key, 'function' : 'cancel_order', 'id_order' : id}
+					time.sleep(sleep_time)
+					data = funcs.call(values)
+					if data['status'] == 1:
+						success_cancel = False
+					else:
+						canceled += 1
+			values = {'key' : self.api_key, 'function' : 'send_order', 'id_market' : id_market, 'side' : -1, 'price' : sell_target_price, 'volume' : self.qty}	
+			time.sleep(sleep_time)
+			data = funcs.call(values)
+			if data['status'] == 1:
+				success_send = False
+			else:
+				sent += 1
+			
+		return success_send, sent, success_cancel, canceled, mid, s
+	
+	
+sleep_time = 2.2
+		
+funcs = la.functions()
+key = 	'8@0S4PYLF187MK5L3U5BWUMKMI70FEMX'#'1\G746OIV9SDMFRS26Z4H9OGM3J1VRYM',#
+agent = Agent(lambda1 = 0.05, lambda2 = 0.05, tau = 1, qty = 10, spread = 0, api_key = key, funcs = funcs)
+
+id_market = 2
+
+status = agent.GetTrades(id_market)
+print 'Fetch trades : ', status
+status = agent.GetMyTrades(id_market)
+print 'Fetch my trades : ', status
+status = agent.GetMyLimits(id_market)
+print 'Fetch my limits : ', status	
+success_send, sent, success_cancel, canceled, mid, spread = agent.DoIt(id_market)
+print 'Mid-price :',  mid, ' Spread : ', spread, ' Position : ', agent.position1, ' PNL : ', agent.pnl
+print 'Send_success : ', success_send, ', Sent : ', sent
+print 'Cancel_success : ', success_cancel, ', Canceled : ', canceled
+agent.balance = agent.GetBalance()
+print 'balance : ', agent.balance
+	
+while(True):
+	time.sleep(1)
+	response = agent.HasChanged()
+	if response:
+		print ''
+		status = agent.GetTrades(id_market)
+		print 'Fetch trades : ', status
+		status = agent.GetMyTrades(id_market)
+		print 'Fetch my trades : ', status
+		status = agent.GetMyLimits(id_market)
+		print 'Fetch my limits : ', status
+		success_send, sent, success_cancel, canceled, mid, spread = agent.DoIt(id_market)
+		print 'Mid-price :',  mid, ' Spread : ', spread, ' Position : ', agent.position1, ' PNL : ', agent.pnl
+		print 'Send_success : ', success_send, ', Sent : ', sent
+		print 'Cancel_success : ', success_cancel, ', Canceled : ', canceled
+		agent.balance = agent.GetBalance()
+		print 'balance : ', agent.balance
+		
+		
+# print obj.get_depth(data['limits'], 3)
+
+# print 'Response: ', response.status, response.reason
+# print 'Data:'
+
+# print data
